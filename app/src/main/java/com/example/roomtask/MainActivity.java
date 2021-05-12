1b634627e8926378a0827bca20a89d2524d2d09d
Sabbir Ahmad <sabbir.sio@gmail.com>
2021-05-13 05:27:41 +0530

    implemented recyler view with room

--- app/src/main/java/com/example/roomtask/MainActivity.java
+++ app/src/main/java/com/example/roomtask/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.roomtask;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
+import com.example.roomtask.adapter.NoteAdapter;
 import com.example.roomtask.entity.Note;
 import com.example.roomtask.viewmodel.NoteViewModel;
 
@@ -21,14 +24,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+        RecyclerView recyclerView = findViewById(R.id.recyler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter noteAdapter = new NoteAdapter();
+        recyclerView.setAdapter(noteAdapter);
+
         noteViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication())).get(NoteViewModel.class);
 
         noteViewModel.getNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
-                //update recycler view
                 Log.i("ON_CHANGE", "On chagned method excuted");
+
+                noteAdapter.setNotes(notes);
             }
         });
     }
